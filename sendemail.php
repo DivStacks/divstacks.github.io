d23003d4b0e45af8f6bc6790a15c859cad7abe8c
DivStacks <stackdiv@gmail.com>
2016-08-02 00:09:43 +0500

    Tokens table

--- sendemail.php
+++ sendemail.php
@@ -3,7 +3,7 @@ $name       = @trim(stripslashes($_POST['name']));
 $from       = @trim(stripslashes($_POST['email'])); 
 $subject    = @trim(stripslashes($_POST['subject'])); 
 $message    = @trim(stripslashes($_POST['message'])); 
-$to   		= 'email@email.com';//replace with your email
+$to   		= 'stackdiv@gmail.com';//replace with your email
 
 $headers   = array();
 $headers[] = "MIME-Version: 1.0";
@@ -16,3 +16,4 @@ $headers[] = "X-Mailer: PHP/".phpversion();
 mail($to, $subject, $message, $headers);
 
 die;
+ ?>
